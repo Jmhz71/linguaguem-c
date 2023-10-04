6a7d8efbc7085f950442d5b8026b7fa5e8d70db1
japinha do met <91266348+Jmhz71@users.noreply.github.com>
2023-10-04 11:27:48 -0300

    Create votação.c

---
+++ votação.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+
+
+int main () {
+
+    int idade;
+
+      setlocale(LC_ALL,"portuguese");
+
+
+
+
+
+
+
+
+
+
+      
+/*
+
+    printf("digite sua idade:");
+    scanf("%d",&idade);
+
+    if (idade >= 18 && idade <= 65){
+        printf ("obrigado a votar!");
+    } else {
+      printf ("Não é obrigado a votar!");
+    }
+*/
+
+   return 0;
+}
+    
